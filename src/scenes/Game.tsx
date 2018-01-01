18d7fef3ae2313686082a28a4bf21a541ceb4ab4
Rafael Poveda <raerpo@gmail.com>
2018-01-01 16:53:45 -0300

    add styles for the hover effect

--- src/scenes/Game.tsx
+++ src/scenes/Game.tsx
@@ -20,6 +20,33 @@ const CountriesWrapper = styled.div`
 
 const Country = styled.div`
   background-color: ${props => (props.color ? props.color : "white")};
+  cursor: pointer;
+  overflow: hidden;
+  &:hover {
+    & > p:before {
+      transform: scale(10);
+    }
+  }
+  & > p {
+    text-shadow: 0 3px 0 rgba(1,1,1,0.1);
+    font-size: 5rem;
+    margin: 0px auto;
+    text-align: center;
+    margin-top: 30vh;
+    max-width: 60%;
+    position: relative;
+    &:before {
+      content: '';
+      width: 2em;
+      height: 2em;
+      background-color: rgba(255,255,255,0.2);
+      border-radius: 50%;
+      position: absolute;
+      left: calc(50% - 1em);
+      top: -.5em;
+      transition: all .3s ease-in-out;
+    }
+  }
 `;
 
 const Capital = styled.div`
@@ -30,18 +57,25 @@ const Capital = styled.div`
   position: absolute;
   margin-left: calc(50% - 10vw);
   text-align: center;
-  color: black;
   margin-top: calc(50vh - 10vw);
+  & > p {
+    text-shadow: 0 3px 0 rgba(1,1,1,0.1);
+    margin: 0px;
+    margin-top: 6rem;
+    padding: 1em;
+    font-size: 1.5em;
+    color: black;
+  }
 `;
 
 const Game = (props: gameProps) => {
   const { colors, capital, rightCountry, wrongCountry } = props;
   return (
     <GameWrapper>
-      <Capital>{capital}</Capital>
+      <Capital><p>From which country is <br /> {capital} <br /> the capital?</p></Capital>
       <CountriesWrapper>
-        <Country color={colors[0]}>{rightCountry}</Country>
-        <Country color={colors[1]}>{wrongCountry}</Country>
+        <Country color={colors[0]}><p>{rightCountry}</p></Country>
+        <Country color={colors[1]}><p>{wrongCountry}</p></Country>
       </CountriesWrapper>
     </GameWrapper>
   );
